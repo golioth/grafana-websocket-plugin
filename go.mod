68e8a9a76b202640f1621cc1e1afafeb6ddccef9
miguel <miguel91@hotmail.com>
2021-12-09 12:10:05 -0300

    feat: creating the websocket plugin for grafana

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/grafana/grafana-starter-datasource-backend
+
+go 1.16
+
+require (
+	github.com/gorilla/websocket v0.0.0-20170926233335-4201258b820c
+	github.com/grafana/grafana-plugin-sdk-go v0.120.0
+)
---
+++ src/ConfigEditor.tsx
@@ -0,0 +1,83 @@
+import React, { ChangeEvent, PureComponent } from 'react';
+import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
+import { MyDataSourceOptions, MySecureJsonData } from './types';
+import { LegacyForms } from '@grafana/ui';
+
+const { SecretFormField, FormField } = LegacyForms;
+
+interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
+
+interface State {}
+
+export class ConfigEditor extends PureComponent<Props, State> {
+  onHostChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    const jsonData = {
+      ...options.jsonData,
+      host: event.target.value,
+    };
+    onOptionsChange({ ...options, jsonData });
+  };
+
+  // Secure field (only sent to the backend)
+  onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonData: {
+        apiKey: event.target.value,
+      },
+    });
+  };
+
+  onResetAPIKey = () => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonFields: {
+        ...options.secureJsonFields,
+        apiKey: false,
+      },
+      secureJsonData: {
+        ...options.secureJsonData,
+        apiKey: '',
+      },
+    });
+  };
+
+  render() {
+    const { options } = this.props;
+    const { jsonData, secureJsonFields } = options;
+    const secureJsonData = (options.secureJsonData || {}) as MySecureJsonData;
+
+    return (
+      <div className="gf-form-group">
+        <div className="gf-form">
+          <FormField
+            label="Host"
+            labelWidth={10}
+            inputWidth={30}
+            onChange={this.onHostChange}
+            value={jsonData.host || ''}
+            placeholder="https://localhost:9080"
+          />
+        </div>
+
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <SecretFormField
+              isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
+              value={secureJsonData.apiKey || ''}
+              label="API Key"
+              placeholder="secure field (backend only)"
+              labelWidth={10}
+              inputWidth={30}
+              onReset={this.onResetAPIKey}
+              onChange={this.onAPIKeyChange}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
