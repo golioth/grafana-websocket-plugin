ba163f3e730c877c4114a9e052f82c3fc9f2ec85
Rodrigo Chireia <rodrigochireia@gmail.com>
2022-01-07 14:53:13 -0400

    feat: add separated query parameters and custom headers on QueryEditor

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -2,6 +2,8 @@ import { DataSourcePluginOptionsEditorProps } from '@grafana/data'
 import { LegacyForms } from '@grafana/ui'
 import React, { ChangeEvent, FC } from 'react'
 import { DataSourceOptions, SecureJsonData } from '../types'
+import CustomHeadersSettings from './fields/CustomHeadersField'
+import CustomQueryParamsSettings from './fields/CustomQueryParamsField'
 
 const { SecretFormField, FormField } = LegacyForms
 
@@ -44,33 +46,29 @@ export const ConfigEditor: FC<Props> = ({ options, onOptionsChange }) => {
 
   return (
     <div className='gf-form-group'>
-      <div className='gf-form'>
-        <FormField
-          label='Host'
-          labelWidth={10}
-          inputWidth={30}
-          onChange={onHostChange}
-          value={jsonData.host || ''}
-          placeholder='https://localhost:9080'
-        />
-      </div>
-
-      <div className='gf-form-inline'>
+      <h3 className='page-heading'>WebSocket</h3>
+      <div className='gf-form-group'>
         <div className='gf-form'>
-          <SecretFormField
-            isConfigured={
-              (secureJsonFields && secureJsonFields.apiKey) as boolean
-            }
-            value={secureJsonData.apiKey || ''}
-            label='API Key'
-            placeholder='secure field (backend only)'
+          <FormField
+            label='Host'
             labelWidth={10}
             inputWidth={30}
-            onReset={onResetAPIKey}
-            onChange={onAPIKeyChange}
+            onChange={onHostChange}
+            value={jsonData.host || ''}
+            placeholder='wss://api.domain.io/v1/ws/'
           />
         </div>
       </div>
+
+      <CustomHeadersSettings
+        dataSourceConfig={options}
+        onChange={onOptionsChange}
+      />
+
+      <CustomQueryParamsSettings
+        dataSourceConfig={options}
+        onChange={onOptionsChange}
+      />
     </div>
   )
 }
